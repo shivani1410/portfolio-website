7e03bfd4e5a5fb08d17a92ac00459e8908f8cd35
shivani1410 <shivanimathur1410@gmail.com>
2025-11-15 02:34:34 +0530

    updated lets connect

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-
+import emailjs from 'emailjs-com';
 @Component({
   selector: 'app-contact',
-  imports: [MatButtonModule, MatFormFieldModule, MatInputModule],
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule,FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
-
+   username: string = '';
+  email: string = '';
+  message: string = '';
+sendEmail() {
+ 
+  emailjs.send(
+    'service_cy8roh2',
+    'template_zko3pcr',
+    {
+      from_name: this.username,
+      reply_to: this.email,
+      message: this.message,
+    },
+    'BA4SLGEc1vZGO5W9F'
+  )
+  .then(() => alert("Message sent!"))
+  .catch(() => alert("Failed to send"));
+}
 }
